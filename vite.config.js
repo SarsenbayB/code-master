fdd2feeaca4666191e9d6308557519273e3f648d
Bakbergen Sarsenbay <bakbergen.sarsenbay@mail.ru>
2026-04-22 07:52:56 +0500

    fix:  Vite static copy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  publicDir: 'public',
   server: {
-    open: '/register1.html', // Автоматты ашылатын бет
+    open: '/register1.html',
   },
   build: {
     rollupOptions: {
       input: {
-        main: 'register1.html', // Негізгі кіру нүктесі
+        main: 'register1.html',
         login: 'index1.html',
         dashboard: 'dashboard.html',
       },
